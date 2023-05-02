e80625d23b0d5229010c60551d4f96a9e7b51b79
chsonam <126577346+chsonam@users.noreply.github.com>
2023-05-02 15:41:45 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://a2dd8f4fe4265469c97db8fb4f68d5fa-606287383.ap-south-1.elb.amazonaws.com"
+  private baseUrl="http://a38bc672205204f2ebb86a3405c449d2-47717157.ap-south-1.elb.amazonaws.com"
 
   constructor(private httpClient:HttpClient) { }
 
